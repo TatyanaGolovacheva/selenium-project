1535be8c8a74adfe05ff0a455850b90698f5bdb6
TatyanaGolovacheva <anhydrone@gmail.com>
2019-07-02 21:14:04 +0300

    тест на открытие/закрытие браузера

---
+++ src/test/java/OpenPageTest.java
@@ -0,0 +1,22 @@
+import junit.framework.TestCase;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+public class OpenPageTest extends TestCase {
+
+
+    @Test
+    public void testOpenPage() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\tgolovacheva\\Documents\\GitHub\\testproject\\src\\main\\resources\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("http://software-testing.ru/");
+        driver.close();
+    }
+
+
+}
