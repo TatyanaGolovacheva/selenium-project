f9f6d810fe61c02972b1fabe862479fc0689d514
TatyanaGolovacheva <anhydrone@gmail.com>
2019-07-04 13:29:53 +0300

    тест логина на страницу http://localhost/litecart/admin/

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -4,19 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-
-public class OpenPageTest extends TestCase {
+public class LoginPageTest extends TestCase {
 
     @Test
-    public void testOpenPage() {
+    public void testLoginPage() {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\tgolovacheva\\Documents\\GitHub\\testproject\\src\\main\\resources\\geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("http://software-testing.ru/");
-
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
         driver.close();
     }
-
 
 }
